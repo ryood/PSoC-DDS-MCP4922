4c216ab920d5c2d0efdb717b5ec5a6fd52bd0db2
ryood <kawanabe@gmail.com>
2015-10-06 05:53:46 +0900

    Impl: for Rhythm Machine

--- PSoC_DDS_MCP4922.cydsn/main.c
+++ PSoC_DDS_MCP4922.cydsn/main.c
@@ -7,6 +7,8 @@
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
  * WHICH IS THE PROPERTY OF your company.
  *
+ * 2015.10.06 2ch DACに対応
+ * 2015.10.06 TC_Sampling_TimerのOVをPINに出力
  * 2015.10.03 MCP4922に12bitデータを送信
  * ========================================
 */
@@ -15,60 +17,77 @@
 #include "wavetable.h"
 
 #define SAMPLE_CLOCK (48000.0f)
+#define BPM (120.0f)
+
+// ERROR CODE
+#define ERR_DAC_CHANNEL_OUT_OF_RANGE 0x01
 
 volatile uint32 phaseRegister;
 volatile uint32 tuningWord;
 
-void DACSetVoltage(uint16 value)
+void DACSetVoltage(uint16 value, int channel)
 {
-	LDAC_PIN_Write(1u);
-	SPIM_SpiUartWriteTxData((value >> 8) | 0x30); // Highバイト(0x30=OUTA/BUFなし/1x/シャットダウンなし)
-	SPIM_SpiUartWriteTxData(value & 0xff);
+	Pin_LDAC_Write(1u);
+    switch (channel) {
+    case 0:
+        // Highバイト(0x30=OUTA/BUFなし/1x/シャットダウンなし)
+	    SPIM_DAC_SpiUartWriteTxData((value >> 8) | 0x30);
+        break;
+    case 1:
+        // Highバイト(0x30=OUTB/BUFなし/1x/シャットダウンなし)
+        SPIM_DAC_SpiUartWriteTxData((value >> 8) | 0xB0);
+        break;
+    default:
+        ;
+        // error(ERR_DAC_CHANNEL_OUT_OF_RANGE);
+    }
+        
+	SPIM_DAC_SpiUartWriteTxData(value & 0xff);
 			
-	while(0u == (SPIM_GetMasterInterruptSource() & SPIM_INTR_MASTER_SPI_DONE))
+	while(0u == (SPIM_DAC_GetMasterInterruptSource() & SPIM_DAC_INTR_MASTER_SPI_DONE))
 	{
 		/* Wait while Master completes transfer */
 	}
 	
-    LDAC_PIN_Write(0u);
+    Pin_LDAC_Write(0u);
     
 	/* Clear interrupt source after transfer completion */
-	SPIM_ClearMasterInterruptSource(SPIM_INTR_MASTER_SPI_DONE);
+	SPIM_DAC_ClearMasterInterruptSource(SPIM_DAC_INTR_MASTER_SPI_DONE);
 }
 
-CY_ISR(InterruptHandler)
+CY_ISR(ISR_Sampling_Timer_Handler)
 {
 	// Caluclate Wave Value
+    //
 	phaseRegister += tuningWord;
 
 	// 32bitのphaseRegisterをテーブルの10bit(1024個)に丸める
 	uint32 index = phaseRegister >> 22;
     uint16 waveValue = waveTableSine[index];
 	
-	DACSetVoltage(waveValue);
+	DACSetVoltage(waveValue, 0);
     
-    Sampling_Timer_ClearInterrupt(Sampling_Timer_INTR_MASK_TC);
+    TC_Sampling_Timer_ClearInterrupt(TC_Sampling_Timer_INTR_MASK_TC);
 }
 
 int main()
 {
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     
     // 変数の初期化
-	double waveFrequency = 1000.0f;
+	double waveFrequency = (BPM / 60.0f);
 	tuningWord = waveFrequency * pow(2.0, 32) / SAMPLE_CLOCK;
     phaseRegister = 0;
-    
+       
     // コンポーネントの初期化
-    Sampling_Timer_Start(); 
-    Timer_ISR_StartEx(InterruptHandler);
-    SPIM_Start();
+    TC_Sampling_Timer_Start(); 
+    ISR_Sampling_Timer_StartEx(ISR_Sampling_Timer_Handler);
+    SPIM_DAC_Start();
     
     CyGlobalIntEnable;
     
     for(;;)
     {
-        /* Place your application code here. */
+        
     }
 }
 
