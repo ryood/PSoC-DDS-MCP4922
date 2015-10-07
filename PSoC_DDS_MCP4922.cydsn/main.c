3f79ba83f6cf7f9cd8d16d6bdc924ac472e25350
ryood <kawanabe@gmail.com>
2015-10-08 00:18:00 +0900

    SPI:8MHz Sampling Clock:48kHz 2ch出力

--- PSoC_DDS_MCP4922.cydsn/main.c
+++ PSoC_DDS_MCP4922.cydsn/main.c
@@ -7,6 +7,7 @@
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
  * WHICH IS THE PROPERTY OF your company.
  *
+ * 2015.10.07 2ch同時出力
  * 2015.10.06 2ch DACに対応(2ch同時出力は不可）
  * 2015.10.06 TC_Sampling_TimerのOVをPINに出力
  * 2015.10.03 MCP4922に12bitデータを送信
@@ -17,13 +18,16 @@
 #include "wavetable.h"
 
 #define SAMPLE_CLOCK (48000.0f)
-#define WAVE_FREQUENCY (1.0f)
+#define WAVE_FREQUENCY_A (1000.0f)
+#define WAVE_FREQUENCY_B (2000.0f)
 
 // ERROR CODE
 #define ERR_DAC_CHANNEL_OUT_OF_RANGE 0x01
 
-volatile uint32 phaseRegister;
-volatile uint32 tuningWord;
+volatile uint32 phaseRegisterA;
+volatile uint32 phaseRegisterB;
+volatile uint32 tuningWordA;
+volatile uint32 tuningWordB;
 
 void DACSetVoltage(uint16 value, int channel)
 {
@@ -58,16 +62,27 @@ void DACSetVoltage(uint16 value, int channel)
 
 CY_ISR(ISR_Sampling_Timer_Handler)
 {
-	// Caluclate Wave Value
+    uint32 index;
+    
+	// Caluclate Wave Value A
     //
-	phaseRegister += tuningWord;
-
+	phaseRegisterA += tuningWordA;
+    
 	// 32bitのphaseRegisterをテーブルの10bit(1024個)に丸める
-	uint32 index = phaseRegister >> 22;
-    uint16 waveValue = waveTableSine[index];
+	index = phaseRegisterA >> 22;
+    uint16 waveValueA = waveTableSine[index];
+
+    // Caluclate Wave Value B
+    //
+    phaseRegisterB += tuningWordB;
+
+    index = phaseRegisterB >> 22;
+    uint16 waveValueB = waveTableSine[index];
 	
-	DACSetVoltage(waveValue, 0);
-    DACSetVoltage(waveValue, 1);
+    // Output
+    //
+	DACSetVoltage(waveValueA, 0);
+    DACSetVoltage(waveValueB, 1);
     
     TC_Sampling_Timer_ClearInterrupt(TC_Sampling_Timer_INTR_MASK_TC);
 }
@@ -75,10 +90,11 @@ CY_ISR(ISR_Sampling_Timer_Handler)
 int main()
 {    
     // 変数の初期化
-	//double waveFrequency = (BPM * 4 / 60.0f);
-    double waveFrequency = WAVE_FREQUENCY;
-	tuningWord = waveFrequency * pow(2.0, 32) / SAMPLE_CLOCK;
-    phaseRegister = 0;
+	tuningWordA = WAVE_FREQUENCY_A * pow(2.0, 32) / SAMPLE_CLOCK;
+    phaseRegisterA = 0;
+    
+    tuningWordB = WAVE_FREQUENCY_B * pow(2.0, 32) / SAMPLE_CLOCK;
+    phaseRegisterB = 0;
        
     // コンポーネントの初期化
     TC_Sampling_Timer_Start(); 
